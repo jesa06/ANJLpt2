8f8a919c2d7dedad08176a680df1551ef243671c
NajaAFonseca <najafonseca777@gmail.com>
2023-02-15 14:38:19 -0800

    s

--- model/foryous.py
+++ model/foryous.py
@@ -7,72 +7,79 @@ from sqlalchemy.exc import IntegrityError
 import json
 
 class Activity:
-    def __init__(self, name, hobby, price, duration):
+    def __init__(self, name, hobby, price, duration, location=''):
         self._name = name
         self._hobby = hobby
         self._price = price
         self._duration = duration
+        self._location = location
         
-@property 
-def activity(self):
-    return self._name
+    @property 
+    def activity(self):
+        return self._name
 
-@activity.setter
-def activity(self, name):
-    self._name = name 
+    @activity.setter
+    def activity(self, name):
+        self._name = name 
     
-@property
-def hobby(self):
-    return self._hobby
+    @property
+    def hobby(self):
+        return self._hobby
 
-@hobby.setter
-def hobby(self, hobby):
-    self._hobby = hobby
+    @hobby.setter
+    def hobby(self, hobby):
+        self._hobby = hobby
     
-@property
-def price(self):
-    return self._price
+    @property
+    def price(self):
+        return self._price
 
-@price.setter
-def price(self, price):
-    self._price = price
+    @price.setter
+    def price(self, price):
+        self._price = price
     
-@property
-def duration(self):
-    return self._duration
+    @property
+    def duration(self):
+        return self._duration
 
-@duration.setter
-def duration(self, duration):
-    self._duration = duration
+    @duration.setter
+    def duration(self, duration):
+        self._duration = duration
+
+    @property
+    def location(self):
+        return self._location
+
+    @location.setter
+    def location(self, location):
+        self._location = location
     
-# output content using str(object) in human readable form, uses getter
-def __str__(self):
-    return f'name: "{self._name}", hobby: "{self._hobby}", price: "{self._price}", duration: "{self._duration}"'
+    # output content using str(object) in human readable form, uses getter
+    def __str__(self):
+        return f'name: "{self._name}", hobby: "{self._hobby}", price: "{self._price}", duration: "{self._duration}", location: "{self._location}"'
     
-# output command to recreate the object, uses attribute directly
-def __repr__(self):
-    return f'Activity(name={self._name}, hobby={self._hobby}, price={self._price}, duration={self._duration})'
+    # output command to recreate the object, uses attribute directly
+    def __repr__(self):
+        return f'Activity(name={self._name}, hobby={self._hobby}, price={self._price}, duration={self._duration}, location={self._location})'
 
 if __name__ == "__main__":
     
     #define activity objects
-    a1 = Activity(name='SeaWorld', hobby='park', price= "$109-$200", duration= 'all-day')
-    a2 = Activity(name='Balboa Park', hobby='', price='', duration='')
-    a3 = Activity(name='Del Mar Beach', hobby='', price='', duration='')
-    a4 = Activity(name='La Jolla Beach', hobby='', price='', duration='')
-    a5 = Activity(name='Hotel Del', hobby='', price='', duration='')
-    a6 = Activity(name='', hobby='', price='', duration='')
-    a7 = Activity(name='', hobby='', price='', duration='')
-    a8 = Activity(name='', hobby='', price='', duration='')
-    a9 = Activity(name='', hobby='', price='', duration='')
-    a10 = Activity(name='', hobby='', price='', duration='')
-    a11 = Activity(name='', hobby='', price='', duration='')
-    a12 = Activity(name='', hobby='', price='', duration='')
-    a13 = Activity(name='', hobby='', price='', duration='')   
+    a1 = Activity(name='SeaWorld', hobby='park', price= "$109-$200", duration= 'all-day', location='San Diego')
+    a2 = Activity(name='Balboa Park', hobby='', price='', duration='', location='San Diego')
+    a3 = Activity(name='Del Mar Beach', hobby='', price='', duration='', location='Del Mar')
+    a4 = Activity(name='La Jolla Beach', hobby='', price='', duration='', location='La Jolla')
+    a5 = Activity(name='Hotel Del', hobby='', price='', duration='', location='Coronado')
+    a6 = Activity(name='', hobby='', price='', duration='', location='')
+    a7 = Activity(name='', hobby='', price='', duration='', location='')
+    a8 = Activity(name='', hobby='', price='', duration='', location='')
+    a9 = Activity(name='', hobby='', price='', duration='', location='')
+    a10 = Activity(name='', hobby='', price='', duration='', location='')
+    a11 = Activity(name='', hobby='', price='', duration='', location='')
 
 # pur user objects in list for convenience
 activities = [a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13]
 
 print("Test 1, make a dictionary")
-json_string = json_string = json.dumps([activity.__dict__ for activity in activities]) 
+json_string = json.dumps([activity.__dict__ for activity in activities]) 
 print(json_string)
