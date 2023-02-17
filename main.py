a540db2eb7ec899f111ed4c03ad2552a6930c516
NajaAFonseca <najafonseca777@gmail.com>
2023-02-17 13:57:39 -0800

    s

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ from flask import render_template  # import render_template from "public" flask 
 
 # import "packages" from "this" project
 from __init__ import app, db  # Definitions initialization
-from model.jokes import initJokes
 from model.users import initUsers
 from model.yelp import initYelp
 from model.activity import initActivities
 
 # setup APIs
 from api.covid import covid_api # Blueprint import api definition
-from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.yelp import yelp_api
 from api.activity import activity_api
@@ -21,7 +19,6 @@ from api.activity import activity_api
 from projects.projects import app_projects # Blueprint directory import projects definition
 
 # register URIs
-app.register_blueprint(joke_api) # register api routes
 app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(yelp_api)
@@ -44,7 +41,6 @@ def stub():
 @app.before_first_request
 def activate_job():
     db.init_app(app)
-    initJokes()
     initUsers()
     initYelp()
     initActivities()
