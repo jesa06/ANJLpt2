7f80f4f082bf06e432f7a7556de0c84dc1443e5a
NajaAFonseca <najafonseca777@gmail.com>
2023-02-16 17:18:39 -0800

    s

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from __init__ import app, db  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
 from model.seaworld import initSeaworld
-from model.activity import initActivities
 
 # setup APIs
 from api.covid import covid_api # Blueprint import api definition
