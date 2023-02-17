139bb72358a50140ec87577fff221dfdc3362cd5
NajaAFonseca <najafonseca777@gmail.com>
2023-02-17 14:08:44 -0800

    s

--- api/activity.py
+++ api/activity.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from flask_restful import Api, Resource
 from sqlalchemy.exc import IntegrityError
+from __init__ import app, db
 
 from model.activity import Activity, db
 
